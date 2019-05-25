c24f90dcbf8608411dea9374b9c03835aef4bf58
Ray <ray@rayps.com>
2019-05-25 18:00:21 +0800

    Initial Commit

---
+++ Foo Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foo Screen
+//
+//  Created by Ray on 5/25/19.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
